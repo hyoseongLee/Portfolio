0903eebd770c0718e12f8ab2fb76109d99ba5a99
hyoseong <hyoseong123@naver.com>
2025-11-11 22:31:21 +0900

    Feat: 초기환경 설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Globalstyle from './asset/styles/GlobalStyle';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <Globalstyle />
+        </>
+    );
+};
+
+export default App;
